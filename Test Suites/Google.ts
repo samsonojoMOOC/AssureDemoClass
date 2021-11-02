357962d4cf8d535a0a5992e0d25972ce368afa5b
samson ojo <samson.ojo@gmail.com>
2021-11-02 17:27:33 +0100

    Assure workflow

---
+++ Test Suites/Google.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Google</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>994dae78-53b5-45a5-a2c7-ee9c67225ac2</testSuiteGuid>
+   <testCaseLink>
+      <guid>8b8d0840-936c-44b6-b25e-2faf7aaffb5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NewTestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
